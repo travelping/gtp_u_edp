2bfb06a754f149354dc7041865c9fcd5174b1b95
Andreas Schultz <aschultz@tpip.net>
2017-10-30 11:10:46 +0100

    drop tetrapak support and move to rebar3

--- priv/pre-start.sh
+++ priv/pre-start.sh
@@ -9,7 +9,10 @@ ifup() {
     /sbin/ip -n $1 route add default via $4
 }
 
-ifup grx iv-ens224 172.20.16.90/24 172.20.16.1
-ifup proxy iv-ens256 172.20.16.91/24 172.20.16.1
+ifup grx ens224 172.20.16.90/24 172.20.16.1
+/sbin/ip -n grx addr add 172.20.16.98/24 dev ens224
+/sbin/ip -n grx token set ::10:00:00:5A dev ens224
+ifup proxy ens256 172.20.16.91/24 172.20.16.1
+/sbin/ip -n proxy token set ::10:00:00:5B dev ens256
 
 exit 0
